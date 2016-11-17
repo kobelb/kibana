34b171a2745317f935d2b1792553c954b0f35819
kobelb <brandon.kobel@elastic.co>
2016-11-17 08:49:13 -0500

    Fixing test namespaces

--- src/deprecation/__tests__/create_transform.js
+++ src/deprecation/__tests__/create_transform.js
@@ -2,17 +2,19 @@ import expect from 'expect.js';
 import sinon from 'sinon';
 import createTransform from '../create_transform';
 
-describe('deprecation create transform', function () {
-  it('calls single deprecation in array', function () {
-    const deprecations = [sinon.spy()];
-    createTransform(deprecations)({});
-    expect(deprecations[0].calledOnce).to.be(true);
-  });
+describe('deprecation', function () {
+  describe('createTransform', function () {
+    it('calls single deprecation in array', function () {
+      const deprecations = [sinon.spy()];
+      createTransform(deprecations)({});
+      expect(deprecations[0].calledOnce).to.be(true);
+    });
 
-  it('calls multiple deprecations in array', function () {
-    const deprecations = [sinon.spy(), sinon.spy()];
-    createTransform(deprecations)({});
-    expect(deprecations[0].calledOnce).to.be(true);
-    expect(deprecations[1].calledOnce).to.be(true);
+    it('calls multiple deprecations in array', function () {
+      const deprecations = [sinon.spy(), sinon.spy()];
+      createTransform(deprecations)({});
+      expect(deprecations[0].calledOnce).to.be(true);
+      expect(deprecations[1].calledOnce).to.be(true);
+    });
   });
 });
--- src/deprecation/deprecations/__tests__/rename.js
+++ src/deprecation/deprecations/__tests__/rename.js
@@ -3,61 +3,63 @@ import { noop } from 'lodash';
 import rename from '../rename';
 import sinon from 'sinon';
 
-describe('rename deprecation', function () {
-  it('should rename simple property', function () {
-    const value = 'value';
-    const settings = {
-      before: value
-    };
-
-    rename('before', 'after')(settings);
-    expect(settings.before).to.be(undefined);
-    expect(settings.after).to.be(value);
-  });
-
-  it ('should rename nested property', function () {
-    const value = 'value';
-    const settings = {
-      someObject: {
+describe('deprecation/deprecations', function () {
+  describe('rename', function () {
+    it('should rename simple property', function () {
+      const value = 'value';
+      const settings = {
         before: value
-      }
-    };
+      };
 
-    rename('someObject.before', 'someObject.after')(settings);
-    expect(settings.someObject.before).to.be(undefined);
-    expect(settings.someObject.after).to.be(value);
-  });
+      rename('before', 'after')(settings);
+      expect(settings.before).to.be(undefined);
+      expect(settings.after).to.be(value);
+    });
 
-  it ('should rename property, even when the value is null', function () {
-    const value = null;
-    const settings = {
-      before: value
-    };
+    it ('should rename nested property', function () {
+      const value = 'value';
+      const settings = {
+        someObject: {
+          before: value
+        }
+      };
 
-    rename('before', 'after')(settings);
-    expect(settings.before).to.be(undefined);
-    expect(settings.after).to.be(null);
-  });
+      rename('someObject.before', 'someObject.after')(settings);
+      expect(settings.someObject.before).to.be(undefined);
+      expect(settings.someObject.after).to.be(value);
+    });
 
-  it (`shouldn't log when a rename doesn't occur`, function () {
-    const settings = {
-      exists: true
-    };
+    it ('should rename property, even when the value is null', function () {
+      const value = null;
+      const settings = {
+        before: value
+      };
 
-    const log = sinon.spy();
-    rename('doesntExist', 'alsoDoesntExist')(settings, log);
-    expect(log.called).to.be(false);
-  });
+      rename('before', 'after')(settings);
+      expect(settings.before).to.be(undefined);
+      expect(settings.after).to.be(null);
+    });
 
-  it ('should log when a rename does occur', function () {
-    const settings = {
-      exists: true
-    };
+    it (`shouldn't log when a rename doesn't occur`, function () {
+      const settings = {
+        exists: true
+      };
 
-    const log = sinon.spy();
-    rename('exists', 'alsoExists')(settings, log);
+      const log = sinon.spy();
+      rename('doesntExist', 'alsoDoesntExist')(settings, log);
+      expect(log.called).to.be(false);
+    });
 
-    expect(log.calledOnce).to.be(true);
-    expect(log.args[0][0]).to.match(/exists.+deprecated/);
+    it ('should log when a rename does occur', function () {
+      const settings = {
+        exists: true
+      };
+
+      const log = sinon.spy();
+      rename('exists', 'alsoExists')(settings, log);
+
+      expect(log.calledOnce).to.be(true);
+      expect(log.args[0][0]).to.match(/exists.+deprecated/);
+    });
   });
 });
