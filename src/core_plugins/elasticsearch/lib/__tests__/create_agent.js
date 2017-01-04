98ed3dd51d059a4aa63e8235cab9ce172c1cfd2d
kobelb <brandon.kobel@elastic.co>
2017-01-04 13:38:47 -0500

    Refactoring some tests

--- src/core_plugins/elasticsearch/lib/__tests__/create_agent.js
+++ src/core_plugins/elasticsearch/lib/__tests__/create_agent.js
@@ -11,7 +11,7 @@ describe('plugins/elasticsearch', function () {
       const config = {
         url: 'http://localhost:9200'
       };
-      
+
       const agent = createAgent(config);
       expect(agent).to.be.a(http.Agent);
     });
@@ -26,93 +26,14 @@ describe('plugins/elasticsearch', function () {
 
     it(`uses https.Agent when url's protocol is https and ssl.verificationMode is full`, function () {
       const config = {
-         url: 'https://localhost:9200',
-         ssl: {
-           verificationMode: 'full'
-         }
+        url: 'https://localhost:9200',
+        ssl: {
+          verificationMode: 'full'
+        }
       };
 
       const agent = createAgent(config);
       expect(agent).to.be.a(https.Agent);
-    });
-
-    context('ssl', function () {
-      let config;
-
-      beforeEach(function () {
-        config = {
-          url: 'https://localhost:9200',
-          ssl: {
-            verificationMode: 'full'
-          }
-        };
-      });
-
-      it('sets rejectUnauthorized to false when verificationMode is none', function () {
-        config.ssl.verificationMode = 'none';
-        const agent = createAgent(config);
-        expect(agent.options.rejectUnauthorized).to.be(false);
-      });
-
-      it('sets rejectUnauthorized to true when verificationMode is certificate', function () {
-        config.ssl.verificationMode = 'certificate';
-        const agent = createAgent(config);
-        expect(agent.options.rejectUnauthorized).to.be(true);
-      });
-
-      it('sets checkServerIdentity to not check hostname when verificationMode is certificate', function () {
-        config.ssl.verificationMode = 'certificate';
-        const agent = createAgent(config);
-
-        const cert = {
-          subject: {
-            CN: 'wrong.com'
-          }
-        };
-
-        expect(agent.options.checkServerIdentity).withArgs('right.com', cert).to.not.throwException();
-        const result = agent.options.checkServerIdentity('right.com', cert);
-        expect(result).to.be(undefined);
-      });
-
-      it('sets rejectUnauthorized to true when verificationMode is full', function () {
-        config.ssl.verificationMode = 'full';
-        const agent = createAgent(config);
-
-        expect(agent.options.rejectUnauthorized).to.be(true);
-      });
-
-      it(`doesn't set checkServerIdentity when verificationMode is full`, function () {
-        config.ssl.verificationMode = 'full';
-        const agent = createAgent(config);
-
-        expect(agent.options.checkServerIdentity).to.be(undefined);
-      });
-
-      it(`sets ca when certificateAuthorities are specified`, function () {
-        config.ssl.certificateAuthorities = [__dirname + '/fixtures/ca.crt'];
-
-        const agent = createAgent(config);
-        expect(agent.options.ca).to.contain('test ca certificate\n');
-      });
-
-      it(`sets cert and key when certificate and key paths are specified`, function () {
-        config.ssl.certificate = __dirname + '/fixtures/cert.crt';
-        config.ssl.key = __dirname + '/fixtures/cert.key';
-
-        const agent = createAgent(config);
-        expect(agent.options.cert).to.be('test certificate\n');
-        expect(agent.options.key).to.be('test key\n');
-      });
-
-      it(`sets passphrase when certificate, key and keyPassphrase are specified`, function () {
-        config.ssl.certificate = __dirname + '/fixtures/cert.crt';
-        config.ssl.key = __dirname + '/fixtures/cert.key';
-        config.ssl.keyPassphrase = 'secret';
-
-        const agent = createAgent(config);
-        expect(agent.options.passphrase).to.be('secret');
-      });
     });
   });
 });
