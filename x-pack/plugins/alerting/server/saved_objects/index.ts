7b001815017c91df32268d8bb35dab72def5b0f1
kobelb <brandon.kobel@elastic.co>
2023-08-04 07:17:17 -0400

    Specifying alerting rule forward compatibility schema

--- x-pack/plugins/alerting/server/saved_objects/index.ts
+++ x-pack/plugins/alerting/server/saved_objects/index.ts
@@ -11,6 +11,7 @@ import type {
   SavedObjectsExportTransformContext,
   SavedObjectsServiceSetup,
 } from '@kbn/core/server';
+import { schema } from '@kbn/config-schema';
 import { EncryptedSavedObjectsPluginSetup } from '@kbn/encrypted-saved-objects-plugin/server';
 import { MigrateFunctionsObject } from '@kbn/kibana-utils-plugin/common';
 import { ALERTING_CASES_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
@@ -85,6 +86,53 @@ export function setupSavedObjects(
     convertToMultiNamespaceTypeVersion: '8.0.0',
     migrations: getMigrations(encryptedSavedObjects, getSearchSourceMigrations(), isPreconfigured),
     mappings: alertMappings,
+    modelVersions: {
+      // initial (and current) model version
+      1: {
+        changes: [],
+        schemas: {
+          // FC schema defining the known fields (indexed or not) for this version
+          forwardCompatibility: schema.object(
+            {
+              name: schema.string(),
+              tags: schema.arrayOf(schema.string()),
+              enabled: schema.boolean(),
+              alertTypeId: schema.string(),
+              consumer: schema.string(),
+              legacyId: schema.nullable(schema.string()),
+              schedule: schema.object({
+                interval: schema.string(),
+              }),
+              actions: schema.any(),
+              params: schema.any(),
+              mapped_params: schema.maybe(schema.any()),
+              scheduledTaskId: schema.maybe(schema.nullable(schema.string())),
+              createdBy: schema.nullable(schema.string()),
+              updatedBy: schema.nullable(schema.string()),
+              createdAt: schema.string(),
+              updatedAt: schema.string(),
+              apiKey: schema.nullable(schema.string()),
+              apiKeyOwner: schema.nullable(schema.string()),
+              apiKeyCreatedByUser: schema.maybe(schema.nullable(schema.boolean())),
+              throttle: schema.maybe(schema.nullable(schema.string())),
+              notifyWhen: schema.maybe(schema.nullable(schema.any())),
+              muteAll: schema.boolean(),
+              mutedInstanceIds: schema.arrayOf(schema.string()),
+              meta: schema.maybe(schema.any()),
+              executionStatus: schema.any(),
+              monitoring: schema.maybe(schema.any()),
+              snoozeSchedule: schema.maybe(schema.arrayOf(schema.any())),
+              isSnoozedUntil: schema.maybe(schema.nullable(schema.string())),
+              lastRun: schema.maybe(schema.nullable(schema.any())),
+              nextRun: schema.maybe(schema.nullable(schema.string())),
+              revision: schema.number(),
+              running: schema.maybe(schema.nullable(schema.boolean())),
+            },
+            { unknowns: 'ignore' } // note the `unknown: ignore` which is how we're evicting the unknown fields
+          ),
+        },
+      },
+    },
     management: {
       displayName: 'rule',
       importableAndExportable: true,
